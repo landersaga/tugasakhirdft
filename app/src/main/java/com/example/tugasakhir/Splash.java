b0d7ad9fa81cc984d1b0e605c9631be4b11f9715
Fadhilah Fauzi Rahmat <80461903+landersaga@users.noreply.github.com>
2021-03-25 21:30:12 +0700

    Tugas Akhir

---
+++ app/src/main/java/com/example/tugasakhir/Splash.java
@@ -0,0 +1,16 @@
+package com.example.tugasakhir;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
