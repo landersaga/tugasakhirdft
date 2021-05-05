4af5a09b23b23d01611f6a0d3079cccd25b9035f
Fadhilah Fauzi Rahmat <80461903+landersaga@users.noreply.github.com>
2021-05-06 06:27:05 +0700

    Login beres

--- app/src/main/java/com/example/tugasakhir/Splash.java
+++ app/src/main/java/com/example/tugasakhir/Splash.java
@@ -9,7 +9,7 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, activityLogin.class);
         startActivity(intent);
         finish();
     }
---
+++ app/src/main/java/com/example/tugasakhir/activityLogin.java
@@ -0,0 +1,88 @@
+package com.example.tugasakhir;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class activityLogin extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView b_forgotpassword;
+    private TextView b_register;
+    private Button b_login;
+    private EditText text_email, text_password;
+    private FirebaseAuth mAuth;
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        text_email = (EditText) findViewById(R.id.text_email);
+        text_password = (EditText) findViewById(R.id.text_password);
+        b_register = (TextView) findViewById(R.id.b_register);
+        b_register.setOnClickListener(this);
+        b_forgotpassword = (TextView) findViewById(R.id.b_forgotpassword);
+        b_forgotpassword.setOnClickListener(this);
+        b_login = (Button) findViewById(R.id.b_login);
+        b_login.setOnClickListener(this);
+
+    }
+
+    private void userLogin() {
+        String email = text_email.getText().toString().trim();
+        String password = text_password.getText().toString().trim();
+
+        if (email.isEmpty()){
+            text_email.setError("Email can't be empty!");
+            text_email.requestFocus();
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            text_email.setError("Please input email correctly!");
+            text_email.requestFocus();
+        }
+
+        if (password.isEmpty()){
+            text_password.setError("Password can't be empty!");
+            text_password.requestFocus();
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    startActivity(new Intent(activityLogin.this, MainActivity.class));
+                } else {
+                    Toast.makeText(activityLogin.this, "Failed to login!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.b_forgotpassword:
+                startActivity(new Intent(this, activityForgotPassword.class));
+                break;
+            case R.id.b_register:
+                startActivity(new Intent(this, activityRegister.class));
+                break;
+            case R.id.b_login:
+                startActivity(new Intent(this, MainActivity.class));
+        }
+    }
+}
