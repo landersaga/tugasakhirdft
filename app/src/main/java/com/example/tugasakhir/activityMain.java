065f3ea1de3ef941f2f02833619e5c18d3f4618d
Fadhilah Fauzi Rahmat <80461903+landersaga@users.noreply.github.com>
2021-07-22 15:32:15 +0700

    Semua fungsi kontroling beres

--- app/src/main/java/com/example/tugasakhir/activityMain.java
+++ app/src/main/java/com/example/tugasakhir/activityMain.java
@@ -1,11 +1,18 @@
 package com.example.tugasakhir;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.renderscript.Sampler;
 import android.view.View;
@@ -25,9 +32,13 @@ public class activityMain extends AppCompatActivity implements View.OnClickListe
     private TextView value_ppm;
     private Button button_menu;
     private Button button_lamp;
+    private Button button_wave;
+    private Button button_pump;
     private Firebase mRefpH;
     private Firebase mRefPPM;
     private Firebase mRefLED;
+    private Firebase mRefPump;
+    private Firebase mRefWaveMaker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,23 +48,45 @@ public class activityMain extends AppCompatActivity implements View.OnClickListe
         mRefpH = new Firebase("https://tugasakhirdft-default-rtdb.firebaseio.com/pH_value");
         mRefPPM = new Firebase("https://tugasakhirdft-default-rtdb.firebaseio.com/ppm_value");
         mRefLED = new Firebase("https://tugasakhirdft-default-rtdb.firebaseio.com/led_status");
+        mRefPump = new Firebase("https://tugasakhirdft-default-rtdb.firebaseio.com/pump_status");
+        mRefWaveMaker = new Firebase("https://tugasakhirdft-default-rtdb.firebaseio.com/wave_status");
 
         value_ph = (TextView)findViewById(R.id.value_ph);
         value_ppm = (TextView)findViewById(R.id.value_ppm);
         button_lamp = (Button)findViewById(R.id.button_lamp);
+        button_wave = (Button)findViewById(R.id.button_wave);
+        button_pump = (Button)findViewById(R.id.button_pump);
         button_menu = (Button)findViewById(R.id.button_menu);
         button_menu.setOnClickListener(this);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("Hydra", "My Hydra", NotificationManager.IMPORTANCE_HIGH);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
         //Code untuk mengidentifikasi nilai pH dari Firebase
         mRefpH.addValueEventListener(new ValueEventListener() {
+
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 String valuePh = dataSnapshot.getValue(String.class);
                 int value_Ph = Integer.parseInt(valuePh);
 
                 if (value_Ph <= 10) {
+
                     value_ph.setText(String.valueOf(value_Ph));
                     value_ph.setTextColor(Color.RED);
+
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(activityMain.this, "Hydra")
+                            .setSmallIcon(R.drawable.logohydra).setContentTitle("Hydroponic Automation")
+                            .setContentText("Your pH Level is" + value_Ph)
+                            .setStyle(new NotificationCompat.BigTextStyle().bigText("Please be aware of your hydroponic's pH Level is " + value_Ph))
+                            .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+                    NotificationManagerCompat managerCompat = NotificationManagerCompat.from(activityMain.this);
+                    managerCompat.notify(1, builder.build());
+
                 } else if (value_Ph > 10) {
                     value_ph.setText(String.valueOf(value_Ph));
                     value_ph.setTextColor(getResources().getColor(R.color.GreenMain));
@@ -76,6 +109,16 @@ public class activityMain extends AppCompatActivity implements View.OnClickListe
                 if (value_Ppm <= 600) {
                     value_ppm.setText(String.valueOf(value_Ppm));
                     value_ppm.setTextColor(Color.RED);
+
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(activityMain.this, "Hydra")
+                            .setSmallIcon(R.drawable.logohydra).setContentTitle("Hydroponic Automation")
+                            .setContentText("Your pH Level is" + value_Ppm)
+                            .setStyle(new NotificationCompat.BigTextStyle().bigText("Please be aware of your hydroponic's ppm Level is " + value_Ppm))
+                            .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+                    NotificationManagerCompat managerCompat = NotificationManagerCompat.from(activityMain.this);
+                    managerCompat.notify(1, builder.build());
+
                 } else if (value_Ppm >= 600) {
                     value_ppm.setText(String.valueOf(value_Ppm));
                     value_ppm.setTextColor(getResources().getColor(R.color.GreenMain));
@@ -121,6 +164,83 @@ public class activityMain extends AppCompatActivity implements View.OnClickListe
                     });
                 }
 
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        //Code untuk mati/nyalain pump air tendon
+        mRefPump.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String pumpstatus = dataSnapshot.getValue(String.class);
+                int pump_status = Integer.parseInt(pumpstatus);
+
+                if (pump_status == 0){
+                    button_pump.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            FirebaseDatabase database = FirebaseDatabase.getInstance();
+                            DatabaseReference myref = database.getReference("pump_status");
+
+                            myref.setValue(1);
+                            button_pump.setBackground(getResources().getDrawable(R.drawable.button_pump_mainblue));
+                        }
+                    });
+                }else if (pump_status == 1){
+                    button_pump.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            FirebaseDatabase database = FirebaseDatabase.getInstance();
+                            DatabaseReference myref = database.getReference("pump_status");
+
+                            myref.setValue(0);
+                            button_pump.setBackground(getResources().getDrawable(R.drawable.button_pump));
+                        }
+                    });
+                }
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        //Code untuk mati/nyalain wavemaker di tendon nutrisi
+        mRefWaveMaker.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String wavemakerstatus = dataSnapshot.getValue(String.class);
+                int wavemaker_status = Integer.parseInt(wavemakerstatus);
+
+                if (wavemaker_status == 0){
+                    button_wave.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            FirebaseDatabase database = FirebaseDatabase.getInstance();
+                            DatabaseReference myref = database.getReference("wave_status");
+
+                            myref.setValue(1);
+                            button_wave.setBackground(getResources().getDrawable(R.drawable.button_wave_mainblue));
+                        }
+                    });
+                }else if (wavemaker_status == 1){
+                    button_wave.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            FirebaseDatabase database = FirebaseDatabase.getInstance();
+                            DatabaseReference myref = database.getReference("wave_status");
+
+                            myref.setValue(0);
+                            button_wave.setBackground(getResources().getDrawable(R.drawable.button_wave));
+                        }
+                    });
+                }
             }
 
             @Override
